9d6e57f63f3309c8d385b699ac9eeb68daa6e46d
Bhavana Pendyala <bhavana.pendyala@anywhere.co>
2026-01-02 22:23:09 +0530

    Map creation

---
+++ Map/Creation.js
@@ -0,0 +1,6 @@
+/* Map is a collection of keyed data items, just like an Object. 
+But the main difference is that Map allows keys of any type. 
+Map() can only be constructed with new. 
+Attempting to call it without new throws a TypeError.*/
+
+const m = new Map();
